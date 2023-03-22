d6c1eaa976bcfcf5d48ac020b45639e593bb07a2
Graham Sanders <git@grahamsanders.org>
2023-03-22 10:45:20 -0500

    Added new parallax

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -14,7 +14,7 @@ func _ready():
 #func _process(delta):
 #	pass
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Horizontal movement
 	velocity.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	velocity.x *= speed
