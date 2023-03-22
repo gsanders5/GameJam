1312021b3a8598bbdb33876d2b533999124335fd
Graham Sanders <git@grahamsanders.org>
2023-03-22 06:34:15 -0500

    Added player, tile-maps, and parallax background
    Updates #3 and updates #4

---
+++ scripts/Player.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+export (int) var speed = 250
+export (int) var jump_force = 500
+var velocity = Vector2()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	# Horizontal movement
+	velocity.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	velocity.x *= speed
+
+	# Vertical movement
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force
+	else:
+		velocity.y += 20  # Apply gravity
+
+	# Move character
+	velocity = move_and_slide(velocity, Vector2.UP)
